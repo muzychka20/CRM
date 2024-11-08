78c03b2dda65c318c41b4f103740179f28fda258
Kyrylo Muzychka <kyryll2016@gmail.com>
2024-11-08 17:44:41 +0200

    feat: delete record

--- crm/website/views.py
+++ crm/website/views.py
@@ -64,3 +64,13 @@ def customer_record(request, pk):
     else:
         messages.success(request, "You must be logged in to view that page")
         return redirect('home')
+
+
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, "Record has been deleted!")
+    else:
+        messages.success(request, "You must be logged in to delete!")
+    return redirect('home')
