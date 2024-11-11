d03b1dbe45231948a6aea73800f078fcf125495d
Kyrylo Muzychka <kyryll2016@gmail.com>
2024-11-11 09:49:33 +0200

    feat: add record

--- crm/website/views.py
+++ crm/website/views.py
@@ -2,7 +2,9 @@ from django.contrib.auth import authenticate
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from django.template.context_processors import request
+
+from .forms import SignUpForm, AddRecordForm
 from .models import Record
 
 
@@ -77,4 +79,28 @@ def delete_record(request, pk):
 
 
 def add_record(request):
-    return render(request, 'add_record.html', {})
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            if form.is_valid():
+                add_record = form.save()
+                messages.success(request, "Record added")
+                return redirect('home')
+        return render(request, 'add_record.html', {"form": form})
+    else:
+        messages.success(request, "You must be logged in")
+        return redirect('home')
+
+
+def update_record(request, pk):
+    if request.user.is_authenticated:
+        current_record = Record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance=current_record)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Record has been updated!")
+            return redirect('home')
+        return render(request, 'update_record.html', {"form": form})
+    else:
+        messages.success(request, "You must be logged in")
+        return redirect('home')
